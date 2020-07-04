540ebaa203c70d00b18b5012157d8cd066d0dd53
SansoNoia <devsanso7110@gmail.com>
2020-07-04 23:56:03 +0900

    db 관련

---
+++ db/dir_table.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"os"
+	"io/ioutil"
+)
+
+
+type DirTable struct {
+
+}
+
+
+func ExistDir(path string)bool {
+	
+}
+
+func GetDirFiles(path string) ([]os.FileInfo,error) {
+	
+}
---
+++ db/file_header.go
@@ -0,0 +1,7 @@
+package db
+
+
+
+type FHeader struct {
+	
+}
