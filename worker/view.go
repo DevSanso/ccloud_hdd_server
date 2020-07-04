18167439f544082350477936927853b4dee53839
SansoNoia <devsanso7110@gmail.com>
2020-07-04 23:55:50 +0900

    add ViewDir struct

--- worker/view.go
+++ worker/view.go
@@ -1,17 +1,69 @@
 package worker
 
 import (
+	"ccloud_hdd_server/db"
+	"encoding/json"
+	"bytes"
+	"path/filepath"
 	"net/http"
+	"os"
 )
 
 
-type View struct {}
+type ViewDir struct {}
+
+type _FileInfo struct {
+	Name string
+	Path string
+	IsDir bool
+	Date string
+	Size int64
+}
+
+func convertFI(finfo os.FileInfo,dir_path string) _FileInfo {
+	return _FileInfo{
+		Name : filepath.Base(finfo.Name()),
+		Path : dir_path,
+		IsDir : finfo.IsDir(),
+		Date : finfo.ModTime().Format("2006-01-02"),
+		Size : finfo.Size(),
+	}
+}
 
 
-func (v *View) Do(w http.ResponseWriter,r *http.Request,next Worker) {
+func (v *ViewDir) Do(w http.ResponseWriter,r *http.Request,next Worker) {
 	if r.Method != "GET" {
 		w.WriteHeader(400)
 		return
 	}
-	path := r.URL.Query().Get("path")
+	dirPath := r.URL.Query().Get("dir")
+
+	if !db.ExistDir(dirPath) {
+		w.WriteHeader(404)
+		return
+	}
+	
+
+	iList,err := db.GetDirFiles(dirPath)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	var buf = new(bytes.Buffer)
+	var encoder = json.NewEncoder(buf)
+
+	for _,info := range(iList)  {
+		if err = encoder.Encode(convertFI(info,dirPath));err != nil {break}
+	}
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("content-type","application/json")
+	w.Write(buf.Bytes())
+	w.WriteHeader(200)
+
 }
+
+type FileIconView struct {}
