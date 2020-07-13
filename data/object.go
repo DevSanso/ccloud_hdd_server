8674a1cec3b60c31648e08a9244ba79b9abedc91
SansoNoia <devsanso7110@gmail.com>
2020-07-13 19:38:23 +0900

    파일 입출력: 최우선으로 코딩

--- data/object.go
+++ data/object.go
@@ -1,28 +1,61 @@
 package data
 
-import "context"
+import (
+	"crypto/aes"
+	"os"
+)
 
-const TokenSize = 4096
 
+const FileMode = os.FileMode(0)
+const (
+	AES256 = iota + 1
+)
 type Object struct {
+	*os.File
 	key []byte
+	cryt int
+	tokenSize int64
+	dataSize int64
 }
 
-type DataMeta struct {
-	Name string
-	Offset int64
-	Size int64
-	FullSize int64
-	B []byte
+func NewObject(path string,key []byte,cryt int,tokenSize int64,flag int) (Object,error) {
+	f,err := os.OpenFile(path,flag,FileMode)
+	if err != nil {return Object{nil,nil,0,0},err}
+	return Object{f,key,cryt,tokenSize},nil
 }
 
-func (o *Object) ReadChan() (<-chan DataMeta, context.CancelFunc) {
 
-}
-func (o *Object) WriteChan() chan DataMeta {
+func (o *Object)Read(b []byte)(int,error) {
 
 }
 
-func (o *Object)Size() int64 {
+func (o *Object)GetKey() []byte {
+	return obj.key
+}
+
+func(o *Object)ReadAt()(b []byte,offset int64)(int,error) {
 
 }
+func(o *Object)Write(b []byte)(int,error) {
+
+}
+
+func(o *Object)WriteAt(b []byte,offset int64)(int,error) {
+
+}
+
+func(o *Object)Seek(offset int64, whence int) error {
+
+}
+
+
+
+func(o *Object)FileSize() (int64,error) {
+	i,err := o.File.Stat()
+	if err != nil {return 0,err}
+	return i.Size()
+}
+
+func (o *Object)DataSize() int64 {return o.dataSize}
+func (o *Object)TokenSize() int64 {return o.tokenSize}
+
