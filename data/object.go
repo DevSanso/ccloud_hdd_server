9bb4de64a7a3d6eebed6cd3f439615cd032e18b8
SansoNoia <devsanso7110@gmail.com>
2020-07-15 00:47:36 +0900

    파일 입출력 구현

--- data/object.go
+++ data/object.go
@@ -4,6 +4,7 @@ import (
 	"crypto/aes"
 	"crypto/des"
 	"os"
+	"io"
 	"crypto/cipher"
 	"errors"
 )
@@ -15,25 +16,24 @@ const (
 	DES
 )
 
-var NoReleaseCryptoError = errors.New("NoReleaseCryptoError")
-
+var (
+	NoReleaseCryptoError = errors.New("NoReleaseCryptoError")
+	OverflowSizeError = errors.New("OverflowSizeError")
+	NotMatchArraySizeError = errors.New("NotMatchArraySizeError")
+)
 type Object struct {
 	f *os.File
 	key []byte
+	iv []byte
 	cryt byte
 	tokenSize int64
 	dataSize int64
 }
 
-func NewObject(path string,key []byte,cryt byte,tokenSize,dataSize int64,flag int) (Object,error) {
+func newObject(path string,key []byte,iv []byte,cryt byte,tokenSize,dataSize int64,flag int) (Object,error) {
 	f,err := os.OpenFile(path,flag,FileMode)
-	if err != nil {return Object{nil,nil,0,0,0},err}
-	return Object{f,key,cryt,tokenSize,dataSize},nil
-}
-
-
-func (o *Object)Read(b []byte)(int,error) {
-
+	if err != nil {return Object{nil,nil,nil,0,0,0},err}
+	return Object{f,key,iv,cryt,tokenSize,dataSize},nil
 }
 
 func (o *Object)GetKey() []byte {
@@ -41,26 +41,76 @@ func (o *Object)GetKey() []byte {
 }
 
 func(o *Object)ReadAt(b []byte,offset int64) (int,error) {
+	f_size,err := o.FileSize()
+
+	if err != nil {return 0,err}
+
+	if err = o.checkOffset(offset,f_size); err != nil {return 0,err} 
+
+	buf := make([]byte,o.tokenSize)
+	r_at := io.ReaderAt(o.f)
+	if _,err = r_at.ReadAt(buf,offset);err != nil {return 0,err}
+
+	block,cipher_err := getBlock(o.key,o.cryt)
+	if cipher_err != nil {return 0,cipher_err}
+
+	decrypt(block,buf,b,o.iv)
+	return 0,nil
 }
-func(o *Object)Write(b []byte)(int,error) {
-	
-}
+
 
 func(o *Object)WriteAt(b []byte,offset int64)(int,error) {
+	f_size,err := o.FileSize()
 
+	if err != nil {return 0,err}
+
+	if err = o.checkOffset(offset,f_size); err != nil {return 0,err} 
+
+	buf := make([]byte,o.tokenSize)
+	r_at := io.WriterAt(o.f)
+	if _,err = r_at.WriteAt(buf,offset);err != nil {return 0,err}
+
+	block,cipher_err := getBlock(o.key,o.cryt)
+	if cipher_err != nil {return 0,cipher_err}
+
+	encrypt(block,buf,b,o.iv)
+	return 0,nil
 }
 
-func(o *Object)Seek(offset int64, whence int) error {
+func decrypt(block cipher.Block,src,dst []byte,iv []byte) {
+	cipher.NewCFBDecrypter(block,iv).XORKeyStream(dst,src)
+}
+
+func encrypt(block cipher.Block,src,dst []byte,iv []byte) {
+	cipher.NewCFBEncrypter(block,iv).XORKeyStream(dst,src)
+}
+
+func(o *Object)Seek(offset int64, whence int) (int64,error) {
 	return o.f.Seek(offset,whence)
 }
 
 func(o *Object)Close() error {
-
+	o.key = nil
+	return o.f.Close()
 }
 
-func getCipher(key []byte,cryt byte) (cipher.Block,error) {
+func (o *Object)checkOffset(offset int64,fSize int64) error {
+	if offset + o.tokenSize > fSize {
+		return OverflowSizeError
+	}
+	if offset % o.tokenSize-1 != 0 {
+		return OverflowSizeError
+	}
+	
+	return nil
+	
+}
+
+
+func getBlock(key []byte,cryt byte) (cipher.Block,error) {
 	switch cryt {
 	case AES256:
+		if len(key) != 32 {return nil,NotMatchArraySizeError}
 		return aes.NewCipher(key)
 	case DES:
 		return des.NewCipher(key)
@@ -73,7 +123,7 @@ func getCipher(key []byte,cryt byte) (cipher.Block,error) {
 func(o *Object)FileSize() (int64,error) {
 	i,err := o.f.Stat()
 	if err != nil {return 0,err}
-	return i.Size() 
+	return i.Size(),nil 
 }
 
 func (o *Object)DataSize() int64 {return o.dataSize}
