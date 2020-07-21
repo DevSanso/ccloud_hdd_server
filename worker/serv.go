c85d48db66a23701419076915547ae84ffb65e1d
SansoNoia <devsanso7110@gmail.com>
2020-07-21 11:35:21 +0900

    loop 작성 예정

--- worker/serv.go
+++ worker/serv.go
@@ -2,7 +2,7 @@ package worker
 
 import (
 	"context"
-	"encoding/json"
+	"database/sql"
 	"net/http"
 
 	"path/filepath"
@@ -14,74 +14,88 @@ import (
 	"ccloud_hdd_server/auth"
 	"ccloud_hdd_server/data"
 	"ccloud_hdd_server/db_sql"
+	db_user "ccloud_hdd_server/db_sql/user"
+	"ccloud_hdd_server/file"
 	"ccloud_hdd_server/get_db"
+
 	err_msg "ccloud_hdd_server/worker/internal"
 )
 
-func mergePath(dir, file string)string {
+func mergePath(dir, file string) string {
 	return filepath.FromSlash(dir + "/" + file)
 }
 
-type FileMeta struct{
-	Name string
-	Offset int64
-	Size int64
+type FileMeta struct {
+	Name     string
+	Offset   int64
+	Size     int64
 	FullSize int64
 }
-type FileDataServ struct {}
+type FileDataServ struct{}
 
-
-func (fvs *FileDataServ)getUserBaseId(key int) (int,error) {
-	c,err := get_db.GetDbConn(context.Background())
-	if err != nil {return 0,err}
-	defer c.Close()
-
-	return db_sql.GetBasePathId(c,key)
+func (fvs *FileDataServ) getUserBaseId(c *sql.Conn,key int) (int, error) {
+	return db_user.GetBasePathId(c, key)
 
 }
-func (fvs *FileDataServ)getObjectHeader(path string)(db_sql.Header,error) {
-	c,err := get_db.GetDbConn(context.Background())
-	if err != nil {return nil,err}
-	defer c.Close()
-	return db_sql.LoadObjectHeader(c,path)
+func (fvs *FileDataServ) getHeader(c *sql.Conn,path string) (*db_sql.Header, error) {
+	return db_sql.LoadHeader(c, path)
 }
 
-func (fvs *FileDataServ)Do(w http.ResponseWriter,r *http.Request,key []byte) {
-	ck,ck_err := r.Cookie("session")
-	if ck_err != nil {err_msg.NotLoginResponse(w);return}
-	var using_key int
-	using_key,ck_err = strconv.Atoi(ck.Value)
-
-	if ck_err != nil {err_msg.BadCookieValueResponse(w);return}
-	if using_key,ck_err =auth.GetUesrId(uint32(using_key));ck_err != nil {
-		err_msg.BadCookieValueResponse(w)
-		return;
+func (fvs *FileDataServ) Do(w http.ResponseWriter, r *http.Request, key []byte) {
+	ck, ck_err := r.Cookie("session")
+	if ck_err != nil {
+		err_msg.NotLoginResponse(w)
+		return
 	}
+	var using_key int
+	using_key, ck_err = strconv.Atoi(ck.Value)
 
-	
-
+	if ck_err != nil {
+		err_msg.BadCookieValueResponse(w)
+		return
+	}
+	if using_key, ck_err = auth.GetUesrId(uint32(using_key)); ck_err != nil {
+		err_msg.BadCookieValueResponse(w)
+		return
+	}
 
 	file_name := r.URL.Query().Get("file")
 	dir_path := r.URL.Query().Get("dir")
+	c, db_err := get_db.GetDbConn(context.Background())
+	if db_err != nil {
+		err_msg.CantConnectDbResponse(w);return
+	}
+	var header *db_sql.Header
+	header, db_err = fvs.getHeader(c,dir_path + "/" + file_name)
+	if db_err != nil {
+		err_msg.CantSearchDataResponse(w)
+		return
+	}
+	var iv []byte
+	iv,db_err = db_user.GetUserIv(c,using_key)
+	if db_err != nil {
+		err_msg.CantSearchDataResponse(w)
+		return
+	}
 
-	header,db_err := fvs.getObjectHeader(dir_path+"/"+file_name)
-	if db_err != nil {err_msg.CantSearchDataResponse(w);return}
+	fs := afero.NewBasePathFs(afero.NewOsFs(), header.BaseDir())
+	path := filepath.Join(header.SubDir(), header.Name())
 
-	fs := afero.NewBasePathFs(afero.NewOsFs(),header.BaseDir())
-	path := filepath.Join(header.SubDir(),header.Name())
-	f,f_err :=fs.Open(path)
-	if f_err != nil {err_msg.BadCookieValueResponse(w);return}
+	obj , f_err := file.ReadFile(fs,path,key,iv,header)
+	if f_err != nil {
+		err_msg.BadCookieValueResponse(w)
+		return
+	}
 
 	upgrader := websocket.Upgrader{}
-	ws_conn ,ws_err := upgrader.Upgrade(w,r,nil)
-	if ws_err != nil {err_msg.CantStartWSLoopResponse(w);return}
-	fvs.loop(ws_conn,f,key)
-	
-}
-func(fvs *FileDataServ)loop(conn *websocket.Conn,f afero.File,key []byte) {
-	for {
-
+	ws_conn, ws_err := upgrader.Upgrade(w, r, nil)
+	if ws_err != nil {
+		err_msg.CantStartWSLoopResponse(w)
+		return
 	}
+	fvs.loop(ws_conn, obj, key)
+
 }
+func (fvs *FileDataServ) loop(conn *websocket.Conn, obj *data.Object, key []byte) {
 
-
+}
