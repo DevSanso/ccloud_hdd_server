0dbf44985bef5bdab56fd721596a7ae5a9268a9c
SansoNoia <devsanso7110@gmail.com>
2020-07-21 11:31:41 +0900

    소스코드 별도의 패키지 분리

--- db_sql/user/user.go
+++ db_sql/user/user.go
@@ -1,36 +1,58 @@
-package db_sql
+package user
 
 import (
-	"database/sql"
 	"context"
-
+	"crypto/aes"
+	"database/sql"
+	"errors"
+	"runtime"
 
 	hash "ccloud_hdd_server/use_hash"
 )
 
 
 const (
-	_InsertUserPwSql = "INSERT INTO user(p_hash) VALUES(?);"
+	_InsertUserPwSql = "INSERT INTO user(p_hash,iv) VALUES(?,?);"
 	_InsertUserBaseSql = "INSERT INTO user(base_id) VALUES(?) WHERE id = ?;"
 	_SelectUserSql = "SELECT id FROM user WHERE p_hash = ?;"
 	_SelectUserBaseIdSql = "SELECT base_id FROM user WHERE id = ?;"
 	_CreateUserTableSql = "CREATE TABLE user(" +
 							"id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY," + 
 							"p_hash CHAR(32) NOT NULL," +
+							"iv VARCHAR(16) NOT NULL," +
 							"base_id INTEGER);"
+	_SelectUserIvSql = "SELECT iv FROM user WHERE id = ?;"
+)
+
+
+var (
+	NotMatchBlockSizeErr = errors.New("NotMatchBlockSizeErr")
 )
 
 
 
-
-
-
-func InsertPassword(conn *sql.Conn,passwdString string) error  {
+func InsertUser(conn *sql.Conn,passwdString string,iv []byte) error  {
 	p_pass := []byte(passwdString)
 	p_hash := hash.Sum(p_pass)
 
-	_,err := conn.QueryContext(context.Background(),_InsertUserPwSql,p_hash)
+	if len(iv) != aes.BlockSize {
+		return NotMatchBlockSizeErr
+	}
+	_,err := conn.QueryContext(context.Background(),_InsertUserPwSql,p_hash,string(iv))
 	return err
+}
+
+func GetUserIv(conn *sql.Conn,userId int) ([]byte,error) {
+	var res string
+	row := conn.QueryRowContext(context.Background(),_SelectUserIvSql,userId)
+	err := row.Scan(&res)
+	b_res := []byte(res)
+	if len(b_res) != aes.BlockSize {
+		pc,_,_,_ :=runtime.Caller(5)
+		fun :=runtime.FuncForPC(pc)
+		panic("user iv block size, crypto blocksize not match:" + fun.Name())
+	}
+	return b_res,err
 }
 
 func GetUserId(conn *sql.Conn,passwdHash []byte) (int,error) {
