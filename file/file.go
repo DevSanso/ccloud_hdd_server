6e4a67605671c59c9346eb196f811c77e017d5b8
SansoNoia <devsanso7110@gmail.com>
2020-07-21 11:35:02 +0900

    read,write만 구현

--- file/file.go
+++ file/file.go
@@ -1,20 +1,27 @@
 package file
 
 import (
-	"database/sql"
+	"os"
 	"github.com/spf13/afero"
-	"ccloud_hdd_server/db_sql"
+
 	"ccloud_hdd_server/data"
+	"ccloud_hdd_server/db_sql"
+	"ccloud_hdd_server/use_hash"
+
+
 )
 
 
-func ReadFile(conn *sql.Conn,fs afero.Fs, name string) (*data.Object,error)  {
-
+func ReadFile(fs afero.Fs, name string,key []byte,iv[]byte, header *db_sql.Header) (*data.Object,error)  {
+	hash_name := string(use_hash.Sum([]byte(name)))
+	f,err:=fs.Open(hash_name)
+	if err != nil {return nil,err}
+	return data.NewObject(f,key,iv,data.AES256,header.TokenSize(),header.Size())
 }
-func WriteFile(conn *sql.Conn,fs afero.Fs, name string) (*data.Object,error)  {
-
+func WriteFile(fs afero.Fs, name string,key []byte,iv[]byte,header *db_sql.Header) (*data.Object,error)  {
+	hash_name := string(use_hash.Sum([]byte(name)))
+	f,err := fs.OpenFile(hash_name,os.O_WRONLY | os.O_CREATE,os.FileMode(0))
+	if err != nil {return nil,err}
+	return data.NewObject(f,key,iv,data.AES256,header.TokenSize(),header.Size())
 }
 
-func CreateFile(conn *sql.Conn,fs afero.Fs, name string) (*data.Object,error)  {
-
-}
