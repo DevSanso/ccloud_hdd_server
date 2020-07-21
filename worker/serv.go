c7a39a7de538e92d35cc2eaa964042c8066393d5
SansoNoia <devsanso7110@gmail.com>
2020-07-22 05:11:46 +0900

    내부 패키지 이름명 변경, serv loop 함수 작성중

--- worker/serv.go
+++ worker/serv.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"database/sql"
 	"net/http"
-
+	"encoding/json"
 	"path/filepath"
 	"strconv"
+	"bytes"
 
 	"github.com/gorilla/websocket"
 	"github.com/spf13/afero"
@@ -18,7 +19,7 @@ import (
 	"ccloud_hdd_server/file"
 	"ccloud_hdd_server/get_db"
 
-	err_msg "ccloud_hdd_server/worker/internal"
+	pkg_internal "ccloud_hdd_server/worker/internal"
 )
 
 func mergePath(dir, file string) string {
@@ -33,29 +34,29 @@ type FileMeta struct {
 }
 type FileDataServ struct{}
 
-func (fvs *FileDataServ) getUserBaseId(c *sql.Conn,key int) (int, error) {
+func (fvs *FileDataServ) getUserBaseId(c *sql.Conn, key int) (int, error) {
 	return db_user.GetBasePathId(c, key)
 
 }
-func (fvs *FileDataServ) getHeader(c *sql.Conn,path string) (*db_sql.Header, error) {
+func (fvs *FileDataServ) getHeader(c *sql.Conn, path string) (*db_sql.Header, error) {
 	return db_sql.LoadHeader(c, path)
 }
 
 func (fvs *FileDataServ) Do(w http.ResponseWriter, r *http.Request, key []byte) {
 	ck, ck_err := r.Cookie("session")
 	if ck_err != nil {
-		err_msg.NotLoginResponse(w)
+		pkg_internal.NotLoginResponse(w)
 		return
 	}
 	var using_key int
 	using_key, ck_err = strconv.Atoi(ck.Value)
 
 	if ck_err != nil {
-		err_msg.BadCookieValueResponse(w)
+		pkg_internal.BadCookieValueResponse(w)
 		return
 	}
 	if using_key, ck_err = auth.GetUesrId(uint32(using_key)); ck_err != nil {
-		err_msg.BadCookieValueResponse(w)
+		pkg_internal.BadCookieValueResponse(w)
 		return
 	}
 
@@ -63,39 +64,80 @@ func (fvs *FileDataServ) Do(w http.ResponseWriter, r *http.Request, key []byte) 
 	dir_path := r.URL.Query().Get("dir")
 	c, db_err := get_db.GetDbConn(context.Background())
 	if db_err != nil {
-		err_msg.CantConnectDbResponse(w);return
+		pkg_internal.CantConnectDbResponse(w)
+		return
 	}
 	var header *db_sql.Header
-	header, db_err = fvs.getHeader(c,dir_path + "/" + file_name)
+	header, db_err = fvs.getHeader(c, dir_path+"/"+file_name)
 	if db_err != nil {
-		err_msg.CantSearchDataResponse(w)
+		pkg_internal.CantSearchDataResponse(w)
 		return
 	}
 	var iv []byte
-	iv,db_err = db_user.GetUserIv(c,using_key)
+	iv, db_err = db_user.GetUserIv(c, using_key)
 	if db_err != nil {
-		err_msg.CantSearchDataResponse(w)
+		pkg_internal.CantSearchDataResponse(w)
 		return
 	}
 
 	fs := afero.NewBasePathFs(afero.NewOsFs(), header.BaseDir())
-	path := filepath.Join(header.SubDir(), header.Name())
-
-	obj , f_err := file.ReadFile(fs,path,key,iv,header)
+	
+	obj, f_err := file.ReadFile(fs,
+		filepath.Join(header.SubDir(), header.Name()), key, iv, header)
 	if f_err != nil {
-		err_msg.BadCookieValueResponse(w)
+		pkg_internal.BadCookieValueResponse(w)
 		return
 	}
 
 	upgrader := websocket.Upgrader{}
 	ws_conn, ws_err := upgrader.Upgrade(w, r, nil)
 	if ws_err != nil {
-		err_msg.CantStartWSLoopResponse(w)
+		pkg_internal.CantStartWSLoopResponse(w)
 		return
 	}
-	fvs.loop(ws_conn, obj, key)
-
+	fvs.loop(ws_conn, obj,header)
 }
-func (fvs *FileDataServ) loop(conn *websocket.Conn, obj *data.Object, key []byte) {
-
+type wsFileRes struct {
+	Name string
+	Size int64
+	Offset int64
+	IsExistNext bool
+	D []byte
 }
+func (fvs *FileDataServ) loop(conn *websocket.Conn, 
+	obj *data.Object,h *db_sql.Header) {
+	defer conn.Close()
+	defer obj.Close()
+
+	offset := int64(0)
+
+	var buf []byte
+	encode := json.NewEncoder(bytes.NewBuffer(buf))
+
+	
+	var data_buf = make([]byte,obj.TokenSize())
+	var res_format = wsFileRes{
+		Name : h.Name(),
+		Size : obj.DataSize(),
+	}
+
+	var isOverDataRange = func() bool {
+		return offset + int64(obj.TokenSize()) > res_format.Size
+	}
+	var cutData = func() []byte {
+		return data_buf[:offset + int64(obj.TokenSize()) - res_format.Size]
+	} 
+
+	for _,err := obj.ReadAt(data_buf,0);
+	 err != nil; _,err = obj.ReadAt(data_buf,offset){
+		if isOverDataRange(){
+				
+			data_buf = data_buf[:]	
+		}
+
+	}
+}
+
+
+
+
--- worker/view_dir_data.go
+++ worker/view_dir_data.go
@@ -2,101 +2,113 @@ package worker
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
 	"os"
-	"context"
 	"strconv"
 
-	
 	"github.com/spf13/afero"
 
 	"ccloud_hdd_server/auth"
-	"ccloud_hdd_server/get_db"
-	db_user "ccloud_hdd_server/db_sql/user"
 	"ccloud_hdd_server/db_sql"
+	db_user "ccloud_hdd_server/db_sql/user"
 	"ccloud_hdd_server/file"
-	err_msg "ccloud_hdd_server/worker/internal"
+	"ccloud_hdd_server/get_db"
+	pkg_internal "ccloud_hdd_server/worker/internal"
 )
 
-
-type ViewDir struct {}
-
-
+type ViewDir struct{}
 
 func convertFI(finfo os.FileInfo) _FileMeta {
 	return _FileMeta{
-		Name : finfo.Name(),
-		IsDir : finfo.IsDir(),
-		Date : finfo.ModTime().Format("2006-01-02"),
-		Size : finfo.Size(),
+		Name:  finfo.Name(),
+		IsDir: finfo.IsDir(),
+		Date:  finfo.ModTime().Format("2006-01-02"),
+		Size:  finfo.Size(),
 	}
 }
 
-
 type _FileMeta struct {
-	Name string
-	Size int64
-	Date string
+	Name  string
+	Size  int64
+	Date  string
 	IsDir bool
 }
 type _FileList struct {
-	Dir string
-	FileInfos []_FileMeta 
+	Dir       string
+	FileInfos []_FileMeta
 }
-func (v *ViewDir)makeFs(key int) (afero.Fs,error) {
-	conn,err := get_db.GetDbConn(context.Background())
-	if err != nil {return nil,err}
+
+func (v *ViewDir) makeFs(key int) (afero.Fs, error) {
+	conn, err := get_db.GetDbConn(context.Background())
+	if err != nil {
+		return nil, err
+	}
 	defer conn.Close()
-	base_key ,sql_err :=db_user.GetBasePathId(conn,key)
-	if sql_err != nil {return nil,sql_err}
+	base_key, sql_err := db_user.GetBasePathId(conn, key)
+	if sql_err != nil {
+		return nil, sql_err
+	}
 	var p string
-	p,sql_err = db_sql.GetBasePath(conn,base_key)
-	if sql_err != nil {return nil,sql_err}
+	p, sql_err = db_sql.GetBasePath(conn, base_key)
+	if sql_err != nil {
+		return nil, sql_err
+	}
 
-	return afero.NewBasePathFs(afero.NewOsFs(),p),nil
+	return afero.NewBasePathFs(afero.NewOsFs(), p), nil
 }
 
-func (v *ViewDir) Do(w http.ResponseWriter,r *http.Request,_ []byte) {
+func (v *ViewDir) Do(w http.ResponseWriter, r *http.Request, _ []byte) {
 	if r.Method != "GET" {
 		w.WriteHeader(400)
 		return
 	}
 
-	ck,ck_err := r.Cookie("session")
-	if ck_err != nil {err_msg.NotLoginResponse(w);return}
+	ck, ck_err := r.Cookie("session")
+	if ck_err != nil {
+		pkg_internal.NotLoginResponse(w)
+		return
+	}
 	var using_key int
-	using_key,ck_err = strconv.Atoi(ck.Value)
+	using_key, ck_err = strconv.Atoi(ck.Value)
 
-	if ck_err != nil {err_msg.BadCookieValueResponse(w);return}
-	if using_key,ck_err =auth.GetUesrId(uint32(using_key));ck_err != nil {
-		err_msg.BadCookieValueResponse(w)
-		return;
+	if ck_err != nil {
+		pkg_internal.BadCookieValueResponse(w)
+		return
+	}
+	if using_key, ck_err = auth.GetUesrId(uint32(using_key)); ck_err != nil {
+		pkg_internal.BadCookieValueResponse(w)
+		return
 	}
 	dir := r.URL.Query().Get("Dir")
-	
-	fs,fs_err:= v.makeFs(using_key)
-	if fs_err != nil {err_msg.CantSearchDataResponse(w);return}
 
-	
+	fs, fs_err := v.makeFs(using_key)
+	if fs_err != nil {
+		pkg_internal.CantSearchDataResponse(w)
+		return
+	}
 
-	iList,err := file.GetFileList(fs,dir)
-	if err != nil {err_msg.CantSearchDataResponse(w);return}
-	
+	iList, err := file.GetFileList(fs, dir)
+	if err != nil {
+		pkg_internal.CantSearchDataResponse(w)
+		return
+	}
+
 	var buf = new(bytes.Buffer)
 	var encoder = json.NewEncoder(buf)
-	
-	var res  = _FileList {}
-	for _,i := range(iList)  {
-		res.FileInfos = append(res.FileInfos,convertFI(i))
+
+	var res = _FileList{}
+	for _, i := range iList {
+		res.FileInfos = append(res.FileInfos, convertFI(i))
 	}
 	err = encoder.Encode(res)
-	if err != nil {err_msg.CantCreateDataResponse(w);return}
+	if err != nil {
+		pkg_internal.CantCreateDataResponse(w)
+		return
+	}
 
-
-	w.Header().Set("content-type","application/json")
+	w.Header().Set("content-type", "application/json")
 	w.Write(buf.Bytes())
 	w.WriteHeader(200)
 }
-
-
