5eecd168c8f5204748a1c33911b3841493960ad7
SansoNoia <devsanso7110@gmail.com>
2020-07-21 11:34:46 +0900

    파일,폴더 전부 읽기로 변경

--- file/dir.go
+++ file/dir.go
@@ -5,18 +5,9 @@ import (
 	"github.com/spf13/afero"
 )
 
-func GetDirList(fs afero.Fs,path string) ([]os.FileInfo,error) {
+func GetFileList(fs afero.Fs,path string) ([]os.FileInfo,error) {
 	f ,err := fs.Open(path)
 	if err != nil {return nil,err}
 	defer f.Close()
-	var finfo []os.FileInfo
-	finfo ,err = f.Readdir(-1)
-	if err != nil {return nil,err}
-
-	var info_arr []os.FileInfo
-	for _,info := range finfo {
-		if !info.IsDir() {continue}
-		info_arr = append(info_arr,info)
-	}
-	return info_arr,err
+	return  f.Readdir(-1)
 }
