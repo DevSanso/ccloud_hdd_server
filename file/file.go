4d3c71606372793a74f9e06d27e389919655b288
SansoNoia <devsanso7110@gmail.com>
2020-07-26 19:10:18 +0900

    add CreateEmptyFile

--- file/file.go
+++ file/file.go
@@ -24,3 +24,11 @@ func WriteFile(fs afero.Fs, name string,key []byte,iv[]byte,header *db_sql.Heade
 	return data.NewObject(f,key,iv,header.TokenSize(),header.Size())
 }
 
+func CreateEmptyFile(fs afero.Fs,name string,key []byte,iv[]byte,tokenSize int) (*data.Object,error) {
+	enc_name,encode_err := EncodeFilePath(key,iv,name)
+	if encode_err != nil {return nil,encode_err}
+	f,err := fs.Create(enc_name)
+	if err != nil {return nil,err}
+	return data.NewObject(f,key,iv,tokenSize,0)
+}
+
