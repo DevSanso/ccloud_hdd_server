729dce8a7ca3087a317f7e3c8c6de4a39bff5a9c
SansoNoia <devsanso7110@gmail.com>
2020-08-04 15:43:14 +0900

    암호에 사용할 key 자동생성 기능 추가

--- pkg/auth/user.go
+++ pkg/auth/user.go
@@ -9,38 +9,53 @@ import (
 
 	"github.com/scylladb/go-set/u32set"
 
+	"ccloud_hdd_server/pkg/use_hash"
 	db_user "ccloud_hdd_server/pkg/db_sql/user"
 )
 
 type userCache struct {
 	session map[uint32]context.Context
+	key map[uint32][]byte
 	cancel  map[uint32]context.CancelFunc
 }
 
 var (
 	NotExistKeyError = errors.New("NotExistKeyError")
+	FullLoginCountErr = errors.New("FullLoginCountErr")
 )
 
 var uc = func() userCache {
 	return userCache{
 		make(map[uint32]context.Context),
+		make(map[uint32][]byte),
 		make(map[uint32]context.CancelFunc),
 	}
 }()
 var sessionKeySet = u32set.New()
 var timeOut = time.Hour * 24
+const max = 1
 
 func Login(conn *sql.Conn, passwd []byte) (uint32, error) {
 	u_id, err := db_user.GetUserId(conn, passwd)
 	if err != nil {
 		return 0, err
 	}
+	if len(uc.session) >= max {
+		return 0,FullLoginCountErr
+	}
+
+	crypto_key, cerr := makeCryptoKey(conn,u_id,passwd)
+	if cerr != nil {
+		return 0,cerr
+	}
 
 	key := makeSessionKey()
 	session := context.WithValue(context.Background(), "uid", u_id)
 	sessTime, cancel := setSessionTime(session)
 
+
 	uc.session[key] = sessTime
+	uc.key[key] = crypto_key
 	uc.cancel[key] = cancel
 
 	return key, nil
@@ -58,7 +73,9 @@ func Logout(key uint32) error {
 
 	return nil
 }
-
+func GetUserCryptoKey(key uint32) []byte {
+	return uc.key[key]
+}
 func GetUesrId(key uint32) (int, error) {
 	if !sessionKeySet.Has(key) {
 		return 0, NotExistKeyError
@@ -67,9 +84,17 @@ func GetUesrId(key uint32) (int, error) {
 	ctx := uc.session[key]
 	return ctx.Value("uid").(int), nil
 }
+func GetLoginUserCount() int {return len(uc.session)}
 
 func setSessionTime(p context.Context) (context.Context, context.CancelFunc) {
 	return context.WithTimeout(p, timeOut)
+}
+
+func makeCryptoKey(conn *sql.Conn,id int,passwd []byte) ([]byte,error) {
+	iv,err := db_user.GetUserIv(conn,id)
+	if err != nil {return nil,err}
+	raw := append(iv,passwd...)
+	return use_hash.Sum(raw),nil
 }
 
 func makeSessionKey() uint32 {
